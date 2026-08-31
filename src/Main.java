a02768377937de13615efae9e19a9ace017a2a18
skelet Przemek <84044411+skeletprzemek@users.noreply.github.com>
2026-08-31 12:41:57 -0400

    finished part C

--- src/Main.java
+++ src/Main.java
@@ -6,47 +6,77 @@ A3: Because the index starts counting at zero, the number of indexes will always
 A4: We would get the NullPointerException because we cannot invoke String.length() because password is null.
 A5: No. "VANIER2026" doesn't contain any lower case characters, which makes the Character.isLowerCase() condition false. In result, upper && lower && digit condition would be false, because if one condition is false the whole thing is considered false.
 A6: No. As in question 5, "vanier2026" doesn't contain any upper case characters, which makes 2 conditions false and does not pass the test.
+
 Part B: Answers
 A1: Initially, the accumulator should be at zero.
 A2: The condition if(Character.isDigit(c));
-A3: After the loop is finished and it reavhes the end of the text.
+A3: After the loop is finished and it reaches the end of the text.
+
+Part C: Answers
+A1: 
 */
 
-void main(){
-    System.out.println(checkPassword("Vanier2026"));
+public class Main {
+    public static void main(){
+    System.out.println(checkPassword("VANIER2026"));
     System.out.println(countDigits("Vanier2026"));
-}
-public static boolean checkPassword(String password){
-
-    boolean upper = false;
-    boolean lower = false;
-    boolean digit = false;
-
-    for (int i = 0; i < password.length(); i++) {
-        char c = password.charAt(i);
-
-        if (Character.isUpperCase(c))
-            upper = true;
-        else if (Character.isLowerCase(c))
-            lower = true;
-        else if (Character.isDigit(c))
-            digit = true;
     }
-    /*
-    System.out.println(upper);
-    System.out.println(lower);
-    System.out.println(digit);
-    System.out.println(upper && lower && digit);
-    */
-    return upper && lower && digit;
-}
-public static int countDigits(String text) {
-    int count = 0;
-    for (int i = 0; i < text.length(); i++) {
-        char c = text.charAt(i);
+    public static boolean checkPassword(String password){
 
-        if (Character.isDigit(c))
-            count++;
+        boolean upper = false;
+        boolean lower = false;
+        boolean digit = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+
+            if (Character.isUpperCase(c))
+                upper = true;
+            else if (Character.isLowerCase(c))
+                lower = true;
+            else if (Character.isDigit(c))
+                digit = true;
+        }
+    
+            /*
+            System.out.println(upper);
+            System.out.println(lower);
+            System.out.println(digit);
+            System.out.println(upper && lower && digit);
+            */
+            return upper && lower && digit;
     }
-    return count;
+    public static int countDigits(String text) {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+
+            if (Character.isDigit(c))
+                count++;
+        }
+        return count;
+    }
+    public static boolean isValidPassword(String password){
+        boolean upper = false;
+        boolean lower = false;
+        boolean digit = false;
+        boolean isValid = false;
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isUpperCase(c))
+                upper = true;
+            else if (Character.isLowerCase(c))
+                lower = true;
+            else if (Character.isDigit(c))
+                digit = true;
+            else if( upper && lower && digit)
+                isValid = true;
+            else
+                isValid = false;
+        }
+            return isValid;
+
+        
+
+    }
 }
